614a29b8bf8a089e1d06222081aba46752449c7a
Severin Ulrich <ulrich.sev@googlemail.com>
2016-04-22 12:45:13 +0200

    Push 2

---
+++ workshops/workshop_01/read.js
@@ -0,0 +1,14 @@
+var fs = require('fs');
+fs.readFile('wolkenkratzer.json', 'utf8', function(err,data) 
+{
+  if (err) throw err;
+  var wolkenData = JSON.parse(data);
+  for(var i = 0; i < wolkenData.length; i++) 
+    { 
+    console.log("Name:"+wolkenData[i].name);
+    console.log("Stadt:"+wolkenData[i].stadt);
+    console.log("Hoehe:"+wolkenData[i].hoehe);
+    console.log("---------------------------------");
+    }
+});
+

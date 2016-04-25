1a4af89ae320b3f632349db2ea384d04c70ca30c
Severin Ulrich <ulrich.sev@googlemail.com>
2016-04-25 13:34:54 +0200

    Push 5
    Aufgabenteil 1-2 fertig gestellt

--- workshops/workshop_01/aufgabe1.js
+++ workshops/workshop_01/aufgabe1.js
@@ -1,13 +1,14 @@
 var fs = require('fs');
+var colour = require('chalk');
 fs.readFile('wolkenkratzer.json', 'utf8', function(err,data) 
 {
   if (err) throw err;
   var wolkenData = JSON.parse(data);
   for(var i = 0; i < wolkenData.wolkenkratzer.length; i++) 
     { 
-    console.log("Name:"+wolkenData.wolkenkratzer[i].name);
-    console.log("Stadt:"+wolkenData.wolkenkratzer[i].stadt);
-    console.log("Hoehe:"+wolkenData.wolkenkratzer[i].hoehe);
+    console.log(colour.red("Name:"+wolkenData.wolkenkratzer[i].name));
+    console.log(colour.yellow("Stadt:"+wolkenData.wolkenkratzer[i].stadt));
+    console.log(colour.green("Hoehe:"+wolkenData.wolkenkratzer[i].hoehe));
     console.log("---------------------------------");
     }
 });

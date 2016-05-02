9b582ddd31acda1f7799c81b7b7e5ff3661a0002
Janek S <Janek.Schuessler@gmail.com>
2016-05-02 10:33:07 +0200

    Push 6
    Finale Version

--- workshops/workshop_01/aufgabe1.js
+++ workshops/workshop_01/aufgabe1.js
@@ -4,7 +4,23 @@ fs.readFile('wolkenkratzer.json', 'utf8', function(err,data)
 {
   if (err) throw err;
   var wolkenData = JSON.parse(data);
-  for(var i = 0; i < wolkenData.wolkenkratzer.length; i++) 
+ wolkenData.wolkenkratzer.sort(function (a,b) {
+    if (a.hoehe > b.hoehe) {
+      return 1;
+    }
+    if (a.hoehe < b.hoehe) {
+      return -1;
+    }
+    return 0;
+  });
+  fs.writeFile(__dirname + "/wolkenkratzer_sortiert.json", JSON.stringify(wolkenData.wolkenkratzer), function(err, data){
+    if (err){
+      console.log(err);
+      return;
+    }
+    console.log("Datei gespeichert!");
+    });
+    for(var i = 0; i < wolkenData.wolkenkratzer.length; i++) 
     { 
     console.log(colour.red("Name:"+wolkenData.wolkenkratzer[i].name));
     console.log(colour.yellow("Stadt:"+wolkenData.wolkenkratzer[i].stadt));
